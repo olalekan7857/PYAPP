66418e2bad5ed331f439124d508ad306c409bcae
olalekan7857 <olalekan7857@gmail.com>
2025-03-14 10:46:10 +0100

    Version 2, calculator

---
+++ app.py
@@ -0,0 +1,12 @@
+score = int(input("What is your score: "))
+
+if score > 80:
+    print("Excellent")
+elif score > 60:
+    print("Very good")    
+elif score > 40:
+    print("Good")    
+elif score > 20:
+    print("Fair")   
+else:
+    print("Fail")     
